842c1116303d8819f153c929c39de20c395c8b9e
Qi Song <rolo_lamperouge@foxmail.com>
2019-11-15 02:28:03 -0500

    add some test script

---
+++ vpcnn/BERT_embed_CNN_script.py
@@ -0,0 +1,13 @@
+"""
+This is a script the run some bert embedding CNN test to make sure the module is runnable
+"""
+import vp_dataset_bert
+import model_bert
+import bert_train
+bert_embedding_path = 'data/bert_embeddings/all.tsv'
+bert_label_embedding_path = 'data/bert_embeddings/labels.tsv'
+train, dev, test = vp_dataset_bert.split(filename=bert_embedding_path,
+                                         label_filename=bert_label_embedding_path,
+                                         num_expert=0)
+model_cnn = model_bert.CNN_Embed()
+bert_train.train(train=train, dev=None,model=model_cnn)
