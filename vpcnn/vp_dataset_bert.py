d9de666e9c02e844e9e777023f7998b1f87c126c
Qi Song <rolo_lamperouge@foxmail.com>
2019-11-15 01:17:17 -0500

    update get item function

--- vpcnn/vp_dataset_bert.py
+++ vpcnn/vp_dataset_bert.py
@@ -18,7 +18,7 @@ class VPDataset_bert_embedding(Dataset):
         self.df = df
 
     def __getitem__(self, index):
-        return self.df.iloc[index]
+        return {'label':self.df.iloc[index]['labels'], 'embed':self.df.iloc[index]['embed']}
 
     def __len__(self):
         return len(list(self.df.index))
