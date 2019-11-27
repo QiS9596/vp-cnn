e4fec661c839873fa16da763579440729c1a53b6
Qi Song <rolo_lamperouge@foxmail.com>
2019-11-27 01:14:45 -0500

    cross validation for single model

--- vpcnn/BERT_embed_CNN_script.py
+++ vpcnn/BERT_embed_CNN_script.py
@@ -8,12 +8,17 @@ bert_embedding_path = 'data/bert_embeddings/all.tsv'
 bert_label_embedding_path = 'data/bert_embeddings/labels.tsv'
 bert_data_npy = 'data/bert_embeddings/all.npy'
 bert_label_npy = 'data/bert_embeddings/labels.npy'
-train, dev, test = vp_dataset_bert.VPDataset_bert_embedding.splits(filename=bert_embedding_path,
+validation_sum = 0.0
+for i in range(10):
+    train, dev, test = vp_dataset_bert.VPDataset_bert_embedding.splits(filename=bert_embedding_path,
+                                                                   foldid=i,
                                                                    label_filename=bert_label_embedding_path,
                                                                    train_npy_name=bert_data_npy,
                                                                    label_npy_name=bert_label_npy,
                                                                    num_experts=0)
-model_cnn = model_bert.CNN_Embed(kernel_num=500)
-acc, model = bert_train.train(train=train, dev=dev,optimizer='adadelta',model=model_cnn,lr=1e-3, epochs=10000, batch_size=100)
+    model_cnn = model_bert.CNN_Embed(kernel_num=400)
+    acc, model = bert_train.train(train=train, dev=dev,optimizer='adadelta',model=model_cnn,lr=1e-3, epochs=1000, batch_size=25)
+    validation_acc = bert_train.eval(test, model, batch_size=50)
+    validation_sum += validation_acc
 print("train seems complete")
-print(acc)
+print(validation_sum/10.0)
