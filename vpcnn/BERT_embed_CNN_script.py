9c3729a1a7640bd0add5cdc66aa59ce65f4eef33
Qi Song <rolo_lamperouge@foxmail.com>
2019-12-03 02:11:09 -0500

    try avg4 embedding

--- vpcnn/BERT_embed_CNN_script.py
+++ vpcnn/BERT_embed_CNN_script.py
@@ -4,8 +4,8 @@ This is a script the run some bert embedding CNN test to make sure the module is
 import vp_dataset_bert
 import model_bert
 import bert_train
-bert_embedding_path = 'data/bert_embeddings/all.tsv'
-bert_label_embedding_path = 'data/bert_embeddings/labels.tsv'
+bert_embedding_path = 'data/bert_embeddings/all_avg4.tsv'
+bert_label_embedding_path = 'data/bert_embeddings/labels_avg4.tsv'
 bert_data_npy = 'data/bert_embeddings/all.npy'
 bert_label_npy = 'data/bert_embeddings/labels.npy'
 validation_sum = 0.0
@@ -16,8 +16,8 @@ for i in range(10):
                                                                    train_npy_name=bert_data_npy,
                                                                    label_npy_name=bert_label_npy,
                                                                    num_experts=0)
-    model_cnn = model_bert.CNN_Embed(kernel_num=500)
-    acc, model = bert_train.train(train=train, dev=dev,optimizer='adadelta',model=model_cnn,lr=1e-2, epochs=500, batch_size=50)
+    model_cnn = model_bert.CNN_Embed(kernel_num=500, embed_dim=768)
+    acc, model = bert_train.train(train=train, dev=dev,optimizer='adadelta',model=model_cnn,lr=5e-3, epochs=1000, batch_size=50)
     validation_acc = bert_train.eval(test, model, batch_size=50)
     validation_sum += validation_acc
 print("train seems complete")
