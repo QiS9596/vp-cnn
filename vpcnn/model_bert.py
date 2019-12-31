28070a934d122921b1a7787f2f44d9152a5bfde7
unknown <rolo_lamperouge@foxmail.com>
2019-12-30 22:53:21 -0800

    start to do auto encoder decoder dimensionality reduction

--- vpcnn/model_bert.py
+++ vpcnn/model_bert.py
@@ -1,20 +1,24 @@
 """
 This module contains CNN network that directly eat bert embeddings, with similar function as model.py
 """
+from abc import ABC, abstractmethod
+
 import torch
 import torch.nn as nn
-from torch.nn import init
 import torch.nn.functional as F
 from torch import autograd
+from torch.nn import init
+
 
 class CNN_Embed(nn.Module):
     """
     This class directly takes word embedding level representation as it's input
     """
+
     def __init__(self,
                  class_num=361,
                  kernel_num=300,
-                 kernel_sizes=[3,4,5],
+                 kernel_sizes=[3, 4, 5],
                  embed_dim=3072,
                  dropout=0.5,
                  conv_init='default',
@@ -51,7 +55,7 @@ class CNN_Embed(nn.Module):
         # it correspond to one dimension in output layer after pooling
         # the output dimensionality of output layer is equal to class_num
         # later a softmax will be applied to this layer
-        self.fc1 = nn.Linear(len(kernel_sizes)*kernel_num,class_num)
+        self.fc1 = nn.Linear(len(kernel_sizes) * kernel_num, class_num)
         if fc_init == 'ortho':
             init.orthogonal(self.fc1.weight.data)
             self.fc1.bias.data.zero_()
@@ -72,18 +76,95 @@ class CNN_Embed(nn.Module):
         x = x.unsqueeze(1)
         x = [F.relu(conv(x)).squeeze(3) for conv in self.convs1]
         x = [F.max_pool1d(i, i.size(2)).squeeze(2) for i in x]
-        x = torch.cat(x,1)
+        x = torch.cat(x, 1)
         x = self.dropout(x)
         linear_out = self.fc1(x)
         return linear_out
+
+
+class BaseAutoEncoderDecoder(nn.Module, ABC):
+    def __init__(self, layers=[3072, 1500, 800, 399]):
+        super(BaseAutoEncoderDecoder, self).__init__()
+        self.neuron_num = layers
+
+    @abstractmethod
+    def encode(self, x):
+        pass
+
+    @abstractmethod
+    def decode(self, x):
+        pass
+
+    @staticmethod
+    def pre_train(mdl, train, optimizer='adadelta', lr=1e-4, batch_size=50, use_cuda=True):
+        pass
+
+
+class AutoEncoderDecoder(BaseAutoEncoderDecoder):
+    def __init__(self, layers=[3072, 1500, 800, 300]):
+        super(AutoEncoderDecoder, self).__init__()
+        _layers = []
+        for i in range(len(layers) - 1):
+            _layers.append(nn.Linear(in_features=layers[i], out_features=layers[i + 1]))
+            _layers.append(nn.ReLU(True))
+        self.encoder = nn.Sequential(*_layers)
+        _layers = []
+        layers.reverse()
+        for i in range(len(layers) - 1):
+            _layers.append(nn.Linear(in_features=layers[i], out_features=layers[i + 1]))
+            _layers.append(nn.ReLU(True))
+        self.decoder = nn.Sequential(*_layers)
+
+    def encode(self, x):
+        x = autograd.Variable(x.float()).cuda()
+        return self.encoder(x)
+
+    def decode(self, x):
+        return self.decode(x)
+
+    def forward(self, x):
+        x = self.encode(x)
+        return self.decode(x)
+
+    @staticmethod
+    def pre_train(mdl, train, optimizer='adadelta', lr=1e-4, batch_size=50, use_cuda=True, epochs=50):
+        if use_cuda:
+            mdl.cuda()
+        if optimizer == 'adadelta':
+            _optimizer = torch.optim.Adadelta(mdl.parameters(), rho=0.95)
+        elif optimizer == 'sgd':
+            _optimizer = torch.optim.SGD(mdl.parameters(), lr=lr)
+        elif optimizer == 'adam':
+            _optimizer = torch.optim.Adam(mdl.parameters(), lr=lr)
+        mdl.train()
+        best_loss = 1000
+        berst_mdl = None
+        # TODO: train autoencoder decoder
+
+
 
 class CNN_shirnk_dim(nn.Module):
     def __init__(self,
                  class_num=361,
                  kernel_num=300,
-                 kernel_sizes=[3,4,5],
+                 kernel_sizes=[3, 4, 5],
                  embed_dim=3072,
                  dropout=0.5,
                  conv_init='default',
-                 fc_init='default'):
-        pass
+                 fc_init='default',
+                 shrinked_dim=300,
+                 shrink_method='s_encoder'):
+        self.cnn_model = CNN_Embed(class_num=class_num,
+                                   kernel_num=kernel_num,
+                                   kernel_sizes=kernel_sizes,
+                                   embed_dim=shrinked_dim,
+                                   dropout=dropout,
+                                   conv_init=conv_init,
+                                   fc_init=fc_init
+                                   )
+        if embed_dim == 3072:
+            auto_encoder_layers = [3072, 1500, 800, 300]
+        elif embed_dim == 768:
+            auto_encoder_layers = [768, 500, 300]
+        if shrink_method == 's_encoder':
+            self.auto_encoder = AutoEncoderDecoder(auto_encoder_layers)
